bba04af8c8be502872d2caee621d6783591f0dd7
gonzaseq <gsequeira@mi.unc.edu.ar>
2026-05-20 19:07:19 -0300

    Update componentes.js

--- componentes.js
+++ componentes.js
@@ -20,8 +20,7 @@
 <nav id="navbar">
   <a href="index.html" class="nav-logo">
   <div class="nav-logo-wrapper">
-  <img src="img/logos1.png" alt="Logo MediSync" class="logo-circle">
-  <img src="img/logo11.png" alt="MediSync Ingeniería Clínica" class="logo-main">
+  <img src="img/logo1.png" alt="MediSync Ingeniería Clínica" class="logo-main">
 </div>
 </a>
 
