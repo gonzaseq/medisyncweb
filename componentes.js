9b9dc5931076d5503424dda1e87a353cd0049c00
gonzaseq <gsequeira@mi.unc.edu.ar>
2026-06-16 14:28:07 -0300

    Update componentes.js

--- componentes.js
+++ componentes.js
@@ -42,7 +42,8 @@
 <div class="nav-mobile-menu" id="mobileMenu">
   <a href="index.html"     onclick="closeMobile()">Inicio</a>
   <a href="servicios.html" onclick="closeMobile()">Servicios</a>
-  <a href="equipos.html"   onclick="closeMobile()">Equipamiento</a>
+  <a href="equipos.html"   onclick="closeMobile()">Cobertura</a>
+  <a href="tienda.html"    onclick="closeMobile()">Tienda</a>
   <a href="nosotros.html"  onclick="closeMobile()">Nosotros</a>
   <a href="contacto.html"  class="nav-cta" onclick="closeMobile()">Solicitar servicio</a>
   <div class="nav-mobile-phones">
